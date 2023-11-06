ff59085017ca00b293921cfd4a944d2b92e42c6d
Kate <kate.burovova@gmail.com>
2023-11-06 18:24:33 +0200

    adding test display

--- app.py
+++ app.py
@@ -1,21 +1,47 @@
 import streamlit as st
 import altair as alt
 import pandas as pd
+#
+# # Example data
+# data = pd.DataFrame({
+#     'a': list('CCCDDDEEE'),
+#     'b': [2, 7, 4, 1, 2, 6, 8, 4, 7]
+# })
+#
+# # Altair chart
+# chart = alt.Chart(data).mark_point().encode(
+#     x='a',
+#     y='b'
+# )
+#
+# # Streamlit page title
+# st.title('My Streamlit App with an Altair Chart')
+#
+# # Display the chart
+# st.altair_chart(chart, use_container_width=True)
 
-# Example data
-data = pd.DataFrame({
-    'a': list('CCCDDDEEE'),
-    'b': [2, 7, 4, 1, 2, 6, 8, 4, 7]
-})
+import streamlit as st
+import altair as alt
+import pandas as pd
 
-# Altair chart
-chart = alt.Chart(data).mark_point().encode(
-    x='a',
-    y='b'
-)
+# Load data from CSV file
+@st.cache  # Add caching so we load the data only once
+def load_data():
+    data = pd.read_csv('data.csv')
+    return data
 
-# Streamlit page title
-st.title('My Streamlit App with an Altair Chart')
+# Use a button to trigger the data loading
+if st.button('Load Data'):
+    data = load_data()
 
-# Display the chart
-st.altair_chart(chart, use_container_width=True)
+    # Create an Altair chart using the loaded data
+    chart = alt.Chart(data).mark_bar().encode(
+        x='a',
+        y='b'
+    )
+
+    # Display the chart
+    st.altair_chart(chart, use_container_width=True)
+else:
+    st.write('Click the button to load data!')
+
